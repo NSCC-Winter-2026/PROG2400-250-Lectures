c1aa3aba9e76fc71f6f1d6af27db3c4ef6b58e54
Brian Shewan <brian.shewan@nscc.ca>
2026-01-13 09:06:25 -0400

    added command line example

---
+++ command_line/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string_view>
+#include <vector>
+
+int main(const int argc, char** argv) {
+
+    const std::vector<std::string_view> args(argv + 1, argv + argc);
+
+    for (const auto& arg : args) {
+        std::cout << arg << std::endl;
+    }
+
+    return 0;
+}
